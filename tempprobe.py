0e3d099fffe5c91dbb64c544d769483e134558d8
r0bi <r0bi@users.noreply.github.com>
2016-11-30 19:20:48 -0600

    Update tempprobe.py
    Latest revision to mirror production code. Client requested only one decimal place of precision and "Probe 1|" to prefix the temperature reading to mirror old temperature probe manufacturer.

--- tempprobe.py
+++ tempprobe.py
@@ -6,16 +6,15 @@ PORT_NUMBER = 8000
 
 sensor = W1ThermSensor()
 
-
 class WebTemp(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(s):
-        #temp_c = sensor.get_temperature()
-        temp_f = sensor.get_temperature(W1ThermSensor.DEGREES_F)
+        #temp_c = sensor.get_temperature() #Celsius, full precision
+        #temp_f = sensor.get_temperature(W1ThermSensor.DEGREES_F) #Fahrenheit, full precision
+        temp_f = str(round(sensor.get_temperature(W1ThermSensor.DEGREES_F),1)) #Fahrenheit, round to one decimal place
         s.send_response(200)
         s.send_header("Content-Type", "text/plain")
         s.end_headers()
-        s.wfile.write(temp_f)
-
+        s.wfile.write("Probe 1|"+temp_f)
 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
